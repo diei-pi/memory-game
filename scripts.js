8c6b782b52b79d1ea44bab02c6417cea43712f9f
diei-pi <jp.chagasrodrigues@gmail.com>
2019-05-08 20:25:03 -0300

    consertado bug de clicar duas vezes na mesma imagem

--- scripts.js
+++ scripts.js
@@ -10,20 +10,23 @@ let carta1,carta2;
 
       cartaVirada=true;
       carta1=this;
+      this.removeEventListener("click",viraCarta);
     }else {
 
       cartaVirada=false;
       carta2=this;
 
     if ( carta1.dataset.framework === carta2.dataset.framework ) {
-
       igual=true;
       carta1.removeEventListener("click",viraCarta);
       carta2.removeEventListener("click",viraCarta);
       }else if(cartaVirada==false) {
         setTimeout(function () {
+          carta1.addEventListener("click",viraCarta);
+          carta2.addEventListener("click",viraCarta);
           carta1.classList.remove("virada");
           carta2.classList.remove("virada");
+          igual=false;
         }, 2000);
       }
     }
