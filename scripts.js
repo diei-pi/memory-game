75e03ea806c078d342439753932a609a775dc3cc
diei-pi <jp.chagasrodrigues@gmail.com>
2019-05-08 20:14:43 -0300

    Cartas escondem novamente após 2 segundos

--- scripts.js
+++ scripts.js
@@ -1,8 +1,32 @@
+const cartas = document.querySelectorAll(".carta");
 
-const cartas = document.querySelectorAll('.carta');
+let cartaVirada=false;
+let carta1,carta2;
 
-function viraCarta() {
-  this.classList.toggle('virada');
-}
+  function viraCarta() {
+    this.classList.add("virada");
 
-cartas.forEach(carta => carta.addEventListener('click', viraCarta));
+    if (cartaVirada==false) {
+
+      cartaVirada=true;
+      carta1=this;
+    }else {
+
+      cartaVirada=false;
+      carta2=this;
+
+    if ( carta1.dataset.framework === carta2.dataset.framework ) {
+
+      igual=true;
+      carta1.removeEventListener("click",viraCarta);
+      carta2.removeEventListener("click",viraCarta);
+      }else if(cartaVirada==false) {
+        setTimeout(function () {
+          carta1.classList.remove("virada");
+          carta2.classList.remove("virada");
+        }, 2000);
+      }
+    }
+  }
+
+cartas.forEach(carta => carta.addEventListener("click", viraCarta));
