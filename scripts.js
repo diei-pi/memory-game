8da7b6cf4a0c804f6ddbe90bb1e06fba1f17b1f9
diei-pi <jp.chagasrodrigues@gmail.com>
2019-05-08 21:33:54 -0300

    resolvido o problema de clicar em varios objetos

--- scripts.js
+++ scripts.js
@@ -1,35 +1,52 @@
 const cartas = document.querySelectorAll(".carta");
 
-let cartaVirada=false;
+let cartaVirada = false;
+let travar = false;
 let carta1,carta2;
 
   function viraCarta() {
+    if (travar) return;
+    if (this === carta1) return;
     this.classList.add("virada");
 
     if (cartaVirada==false) {
 
       cartaVirada=true;
       carta1=this;
-      this.removeEventListener("click",viraCarta);
-    }else {
 
-      cartaVirada=false;
-      carta2=this;
-
-    if ( carta1.dataset.framework === carta2.dataset.framework ) {
-      igual=true;
-      carta1.removeEventListener("click",viraCarta);
-      carta2.removeEventListener("click",viraCarta);
-      }else if(cartaVirada==false) {
-        setTimeout(function () {
-          carta1.addEventListener("click",viraCarta);
-          carta2.addEventListener("click",viraCarta);
-          carta1.classList.remove("virada");
-          carta2.classList.remove("virada");
-          igual=false;
-        }, 2000);
-      }
+      return;
     }
+
+    carta2=this;
+
+      check();
   }
 
+function check(){
+  let igual = carta1.dataset.framework === carta2.dataset.framework;
+
+  igual ? removerclick() : desvirar();
+}
+
+function desvirar(){
+  travar = true;
+
+  setTimeout(function () {
+    carta1.classList.remove("virada");
+    carta2.classList.remove("virada");
+
+    resetTravar();
+  }, 1500);
+
+}
+
+function removerclick(){
+  carta1.removeEventListener("click",viraCarta);
+  carta2.removeEventListener("click",viraCarta);
+  resetTravar();
+}
+function resetTravar(){
+  [cartaVirada, travar] = [false, false];
+  [carta1,carta2] = [null,null];
+}
 cartas.forEach(carta => carta.addEventListener("click", viraCarta));
